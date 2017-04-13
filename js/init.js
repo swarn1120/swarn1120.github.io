bb58cce3de3b6436b60836e4b21e381f840fde8f
swarn1120 <swarn1120@gmail.com>
2017-04-13 17:17:21 -0500

    FRC

--- js/init.js
+++ js/init.js
@@ -1,9 +1,12 @@
 (function($){
   $(function(){
-    $('.scrollspy').scrollSpy();
-    $('.button-collapse').sideNav();
+
+    $(".button-collapse").sideNav();
     $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.modal').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close');
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
