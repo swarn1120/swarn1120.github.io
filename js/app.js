0589dc731ccee8074fe4b7d8ad1a7b9bfffd1f3e
Swarn Singh <swarn2099@gmail.com>
2018-02-13 22:00:36 -0600

    Home page update and finishing up front end

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 function currentLocation(){
 navigator.geolocation.getCurrentPosition(function(location) {
   var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-  L.mapbox.accessToken = 'pk.eyJ1IjoiY3RpcHBldHQiLCJhIjoiS3lpTnN4MCJ9.YG_uH8r7IgwgcSWEPYROMA';
-  var map = L.mapbox.map('map', null, {
-      zoomControl: false,
-      attributionControl: true
-    })
-    .setView(latlng, 18);
-  L.marker([location.coords.latitude, location.coords.longitude]).addTo(map)
-    .bindPopup('Shit will go here')
-  var layers = {
-    Streets: L.mapbox.tileLayer('mapbox.streets'),
-  };
-  layers.Streets.addTo(map);
+    var map = L.map('map', { zoomControl:false }).setView(latlng, 25);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3dhcm4yMDk5IiwiYSI6ImNqZG1nY2ZqMjBtNmQzM285bTF2Y2RhaHkifQ.r-WQDZYDHBF5ahyCamotiA', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets-basic',
+        accessToken: 'pk.eyJ1Ijoic3dhcm4yMDk5IiwiYSI6ImNqZG1nY2ZqMjBtNmQzM285bTF2Y2RhaHkifQ.r-WQDZYDHBF5ahyCamotiA'
+    }).addTo(map);
+
+    L.marker(latlng).addTo(map)
+  .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+  var popup = L.popup();
 
 });
 };
