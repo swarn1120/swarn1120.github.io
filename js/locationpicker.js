5790521bac89a914a49e2a6d227743ea68ed891d
Jerry Huang <jerry@aplomb.co>
2018-02-23 01:13:44 -0600

    Added firebase authentication and realtime database

--- js/locationpicker.js
+++ js/locationpicker.js
@@ -16,10 +16,24 @@ function getForm() {
   } else if (document.getElementById('test3').checked) {
     strCategory = document.getElementById('test3').value;
   }
+
+  var ref = firebase.database().ref();
+  var eventRef = ref.child("Events").child(strEventName.value);
+
+  eventRef.set({
+    eventCreator: "Jerry Huang",
+    eventName: strEventName.value,
+    eventDate: date.value,
+    eventDescription: strDescription.value
+  });
+
+  window.alert("Firebase database updated successfully");
+
   console.log(strEventName.value);
   console.log(date.value);
   console.log(strStartTime.value);
   console.log(endtime.value);
   console.log(strDescription.value);
   console.log(strCategory.value);
+
 }
