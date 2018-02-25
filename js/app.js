14e90e2289d95928a9fc33eadfd4c9c967db6dd5
Swarn Singh <swarn1120@gmail.com>
2018-02-24 18:08:12 -0600

    indian guy 2d array

--- js/app.js
+++ js/app.js
@@ -76,38 +76,86 @@ function login()
 
 }
 
+var markers = [
+    {
+        "title": 'Aksa Beach',
+        "lat": '19.1759668',
+        "lng": '72.79504659999998',
+        "description": 'Aksa Beach is a popular beach and a vacation spot in Aksa village at Malad, Mumbai.'
+    },
+    {
+        "title": 'Juhu Beach',
+        "lat": '19.0883595',
+        "lng": '72.82652380000002',
+        "description": 'Juhu Beach is one of favourite tourist attractions situated in Mumbai.'
+    },
+    {
+        "title": 'Girgaum Beach',
+        "lat": '18.9542149',
+        "lng": '72.81203529999993',
+        "description": 'Girgaum Beach commonly known as just Chaupati is one of the most famous public beaches in Mumbai.'
+    },
+    {
+        "title": 'Jijamata Udyan',
+        "lat": '18.979006',
+        "lng": '72.83388300000001',
+        "description": 'Jijamata Udyan is situated near Byculla station is famous as Mumbai (Bombay) Zoo.'
+    },
+    {
+        "title": 'Sanjay Gandhi National Park',
+        "lat": '19.2147067',
+        "lng": '72.91062020000004',
+        "description": 'Sanjay Gandhi National Park is a large protected area in the northern part of Mumbai city.'
+    }
+    ];
+
 function gotData(data) {
   var values = data.val();
   var keys = Object.keys(values);
-  for (var i = 0; i < keys.length; i++) {
-    var k = keys[i];
-    var eventName = values[k].eventName;
-    var description = values[k].description;
-    var longitude = values[k].longCoord;
-    var latitude = values[k].latCoord;
-    var pos = {
-      lat: latitude,
-      lng: longitude
-    };
-    var marker = new google.maps.Marker({
-      position: pos,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-    var contentString = '<h1 class="title is-4">'+ eventName +'</h1>'+ '<hr color="black">'+
-    '<span>'+ description +'</span><hr color="black">'+
-    '<a class="button is-rounded is-medium theme waves-effect waves-light red lighten-1 white-text">'+
-    '<span class="animated inifinte pulse">&nbsp &nbsp &nbsp Group Chat &nbsp &nbsp &nbsp</span></a>';
-    var infowindow = new google.maps.InfoWindow({
-content: contentString
-});
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-    console.log("Event name: " + eventName);
-    console.log("Description: " + description);
-    console.log("Long: " + longitude);
-    console.log("lat: " + latitude);
+  var infowindow = new google.maps.InfoWindow();
+  for (var i = 0; i < markers.length; i++) {
+    // var k = keys[i];
+    // var eventName = values[k].eventName;
+    // var description = values[k].description;
+    // var longitude = values[k].longCoord;
+    // var latitude = values[k].latCoord;
+    // var pos = {
+    //   lat: latitude,
+    //   lng: longitude
+    // };
+    // var contentString = '<h1 class="title is-4">'+ eventName +'</h1>'+ '<hr color="black">'+
+    // '<span>'+ description +'</span><hr color="black">'+
+    // '<a class="button is-rounded is-medium theme waves-effect waves-light red lighten-1 white-text">'+
+    // '<span class="animated inifinte pulse">&nbsp &nbsp &nbsp Group Chat &nbsp &nbsp &nbsp</span></a>';
+
+    var data = markers[i];
+    var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+           var marker = new google.maps.Marker({
+               position: myLatlng,
+               map: map,
+               title: data.title
+           });
+
+           //Attach click event to the marker.
+           (function (marker, data) {
+               google.maps.event.addListener(marker, "click", function (e) {
+                   //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+                   infowindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                   infowindow.open(map, marker);
+               });
+           })(marker, data);
+
+    // var marker = new google.maps.Marker({
+    //   position: pos,
+    //   map: map,
+    // });
+    // marker.addListener('click', function() {
+    //   infowindow.open(map, marker);
+    // });
+    // console.log("Event name: " + eventName);
+    // console.log("Description: " + description);
+    // console.log("Long: " + longitude);
+    // console.log("lat: " + latitude);
   }
 }
 window.onload = gotData;
