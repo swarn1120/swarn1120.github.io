03efafd4484a06b0ef41d873562d999506a1394d
Swarn Singh <swarn2099@gmail.com>
2018-04-03 01:22:27 -0500

    redirect

--- js/auth.js
+++ js/auth.js
@@ -5,8 +5,12 @@ function googleSignin() {
    firebase.auth().signInWithPopup(provider).then(function(result) {
       var token = result.credential.accessToken;
       var user = result.user;
-      console.log(token)
-      console.log(user)
+      console.log("Success")
+      console.log(result)
+      if(token){
+        window.location = "https/swarn1120.github.io/www/map.html";
+      }
+
    }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
